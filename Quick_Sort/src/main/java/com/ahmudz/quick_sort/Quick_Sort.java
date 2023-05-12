2796b5e167a0c2fd8ed9f852b9bdc93982a5184a
Ahmudul Hossain <ahmudulhossain.n5@gmail.com>
2023-05-12 10:20:54 +0600

    updated

---
+++ Quick_Sort/src/main/java/com/ahmudz/quick_sort/Quick_Sort.java
@@ -0,0 +1,71 @@
+package com.ahmudz.quick_sort;
+
+public class Quick_Sort {
+
+    static int partition(int arr[], int left, int right) {
+        int pivot = left;
+        int minIndex = left + 1;
+        int maxIndex = right;
+
+        while (minIndex <= maxIndex) {
+            while (arr[minIndex] <= arr[pivot]) {
+                minIndex++;
+            }
+
+            while (arr[maxIndex] > arr[pivot]) {
+                maxIndex--;
+            }
+
+            if (maxIndex > minIndex) {
+
+                int temp = arr[maxIndex];
+                arr[maxIndex] = arr[minIndex];
+                arr[minIndex] = temp;
+            }
+        }
+
+        int temp = arr[pivot];
+        arr[pivot] = arr[maxIndex];
+        arr[maxIndex] = temp;
+
+        return maxIndex;
+    }
+
+    static void quickSort(int arr[], int left, int right) {
+        if (left < right) {
+            int pivot_loc = partition(arr, left, right);
+
+            quickSort(arr, left, pivot_loc - 1);
+
+            quickSort(arr, pivot_loc + 1, right);
+
+        }
+    }
+
+    static void printArray(int arr[]) {
+
+        System.out.println("\nsorted array elements: ");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        System.out.println("");
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = {5, 3, 4, 3, 1, 6, 4, 6, 0, 1, 2, 0};
+
+        int arrr[] = {10, 5};
+
+        int temp = arrr[0];
+        arrr[0] = arrr[1];
+        arrr[1] = temp;
+
+        for (int i = 0; i < arrr.length; i++) {
+            System.out.println(arrr[i] + " ");
+        }
+
+    }
+}
