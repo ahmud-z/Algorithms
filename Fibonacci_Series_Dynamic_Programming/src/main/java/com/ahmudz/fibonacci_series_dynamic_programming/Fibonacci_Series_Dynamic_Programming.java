b45ed6dab9d48e80e2a0a1694f259d5ec3b952d0
Ahmudul Hossain <ahmudulhossain.n5@gmail.com>
2023-05-14 09:03:48 +0600

    updated

---
+++ Fibonacci_Series_Dynamic_Programming/src/main/java/com/ahmudz/fibonacci_series_dynamic_programming/Fibonacci_Series_Dynamic_Programming.java
@@ -0,0 +1,22 @@
+package com.ahmudz.fibonacci_series_dynamic_programming;
+
+public class Fibonacci_Series_Dynamic_Programming {
+
+    public static void main(String[] args) {
+
+        int term1 = 0;
+        int term2 = 1;
+        int nextTerm = term1 + term2;
+
+        System.out.print("" + term1 + " " + term2 + " ");
+
+        for (int i = 2; i < 10; i++) {
+
+            System.out.print(nextTerm + " ");
+            term1 = term2;
+            term2 = nextTerm;
+            nextTerm = term1 + term2;
+        }
+
+    }
+}
