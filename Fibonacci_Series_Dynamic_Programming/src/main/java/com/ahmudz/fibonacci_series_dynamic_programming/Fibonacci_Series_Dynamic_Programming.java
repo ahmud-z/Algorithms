6ee87f3964e64a96ceee2b6de5459189ca9de48f
Ahmudul Hossain <ahmudulhossain.n5@gmail.com>
2023-05-20 23:58:46 +0600

    fibonacci using dp

--- Fibonacci_Series_Dynamic_Programming/src/main/java/com/ahmudz/fibonacci_series_dynamic_programming/Fibonacci_Series_Dynamic_Programming.java
+++ Fibonacci_Series_Dynamic_Programming/src/main/java/com/ahmudz/fibonacci_series_dynamic_programming/Fibonacci_Series_Dynamic_Programming.java
@@ -1,22 +1,29 @@
 package com.ahmudz.fibonacci_series_dynamic_programming;
 
+import java.util.Scanner;
+
 public class Fibonacci_Series_Dynamic_Programming {
+
+    static int findFibo(int n) {
+
+        int fib[] = new int[n];
+
+        fib[0] = 0;
+        fib[1] = 1;
+
+        for (int i = 2; i < n; i++) {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+        return fib[n - 1];
+    }
 
     public static void main(String[] args) {
 
-        int term1 = 0;
-        int term2 = 1;
-        int nextTerm = term1 + term2;
+        Scanner input = new Scanner(System.in);
 
-        System.out.print("" + term1 + " " + term2 + " ");
+        System.out.print("Enter no of Fibonacci: ");
+        int n = input.nextInt();
 
-        for (int i = 2; i <= 10; i++) {
-
-            System.out.print(nextTerm + " ");
-            term1 = term2;
-            term2 = nextTerm;
-            nextTerm = term1 + term2;
-        }
-
+        System.out.println(n + " th Fibonacci is: " + findFibo(n)); // 0 1 1 2 3 5 8 13 21 34 55
     }
 }
