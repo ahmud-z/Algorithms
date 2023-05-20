3fcf26c1dae524072a31be7e9beba2bb2f59377d
Ahmudul Hossain <ahmudulhossain.n5@gmail.com>
2023-05-21 05:10:44 +0600

    kruskal algorithm

--- MST_Kruskal_Algorithm/src/main/java/com/ahmudz/mst_kruskal_algorithm/MST_Kruskal_Algorithm.java
+++ MST_Kruskal_Algorithm/src/main/java/com/ahmudz/mst_kruskal_algorithm/MST_Kruskal_Algorithm.java
@@ -1,132 +1,115 @@
 package com.ahmudz.mst_kruskal_algorithm;
 
-import java.util.*;
-import java.lang.*;
-import java.io.*;
+import java.util.Arrays;
+import java.util.Comparator;
+
+class SortByWeight implements Comparator<Edges> {
+
+    public int compare(Edges ob1, Edges ob2) {
+
+        return ob1.weight - ob2.weight;
+    }
+}
+
+class Edges {
+
+    int source;
+    int destination;
+    int weight;
+
+    public Edges() {
+    }
+
+    public Edges(int source, int destination, int weight) {
+        this.source = source;
+        this.destination = destination;
+        this.weight = weight;
+    }
+}
 
 public class MST_Kruskal_Algorithm {
 
-    class Edge implements Comparable<Edge> {
-
-        int src, dest, weight;
-
-        public int comareTo(Edge compareEdge) {
-            return this.weight - compareEdge.weight;
-        }
-    };
-
-    int V, E;
-
-    Edge edge[];
+    int nodeAmount;
+    int edgeAmount;
     int leader[];
 
-    void Kruskal(int v, int e) {
-        V = v;
-        E = e;
-        leader = new int[V];
-        edge = new Edge[E];
-
-        for (int i = 0; i < e; ++i) {
-            edge[i] = new Edge();
-        }
+    public MST_Kruskal_Algorithm(int nodeAmount, int edgeAmount) {
+        this.nodeAmount = nodeAmount;
+        this.edgeAmount = edgeAmount;
+        leader = new int[nodeAmount];
     }
 
-    int Find(int n) {
-        return leader[n];
+    int Find(int u) {
+        return leader[u];
     }
 
     void Union(int rootU, int rootV) {
-
-        int newLeader;
         if (rootU < rootV) {
-            newLeader = rootU;
-            for (int i = 0; i < V; i++) {
+            for (int i = 0; i < nodeAmount; i++) {
                 if (leader[i] == rootV) {
-                    leader[i] = newLeader;
+                    leader[i] = rootU;
                 }
             }
         } else {
-
-            newLeader = rootV;
-            for (int i = 0; i < V; i++) {
-                if (leader[i] == leader[u]) {
-                    leader[i] = newLeader;
+            for (int i = 0; i < nodeAmount; i++) {
+                if (leader[i] == rootU) {
+                    leader[i] = rootV;
                 }
             }
         }
     }
 
-    void kruskalMST() {
+    int kruskalMST(Edges arr[]) {
 
-        Edge result = new Edge(V - 1);
+        Edges resultArray[] = new Edges[nodeAmount - 1];
+        int minCost = 0;
 
-        for (int i = 0; i < result.length; i++) {
-
-            result[i] = new Edge();
+        for (int i = 0; i < resultArray.length; i++) {
+            resultArray[i] = new Edges();
         }
 
-        Arrays.sort(edge);
+        Arrays.sort(arr, new SortByWeight());
 
-        for (int i = 0; i < V; ++i) {
+        for (int i = 0; i < leader.length; i++) {
             leader[i] = i;
         }
 
-        int i = 0;
+        int resultIndex = 0;
 
-        for (int e = 0; e < edge.length; e++) {
+        for (int j = 0; j < arr.length; j++) {
 
-            Edge next_edge = edge[e];
+            Edges nextEdge = arr[j];
 
-            int x = Find(next_edge.src);
-            int y = Find(next_edge.dest);
+            int x = Find(nextEdge.source);
+            int y = Find(nextEdge.destination);
 
             if (x != y) {
-                result[i++] = next_edge;
+                resultArray[resultIndex++] = nextEdge;
                 Union(x, y);
             }
         }
 
-        System.out.println("Folloing are the edges in" + "the constucted MST");
-
-        int minimumCost = 0;
-
-        for (int p = 0; p < result.length; p++) {
-
-            System.out.println(result[p].src + " --- " + result[p].dest + " == " + result[p].weight);
-            minimumCost += result[p].weight;
+        System.out.println("\nSource\tDestination\tWeight");
+        for (int i = 0; i < resultArray.length; i++) {
+            System.out.println(resultArray[i].source + "\t  " + resultArray[i].destination + "\t\t  " + resultArray[i].weight);
+            minCost += resultArray[i].weight;
         }
 
-        System.out.println("Minimum Cost of Spanning Tree: " + minimumCost);
+        return minCost;
     }
 
     public static void main(String[] args) {
 
-        int V = 4;
-        int E = 5;
+        Edges arr[] = {
+            new Edges(0, 1, 10),
+            new Edges(0, 2, 6),
+            new Edges(0, 3, 5),
+            new Edges(1, 3, 15),
+            new Edges(2, 3, 4)
+        };
 
-        MST_Kruskal_Algorithm graph = new MST_Kruskal_Algorithm(V, E);
+        MST_Kruskal_Algorithm ob = new MST_Kruskal_Algorithm(4, 5);
 
-        graph.edge[0].src = 0;
-        graph.edge[0].dest = 1;
-        graph.edge[0].weight = 10;
-
-        graph.edge[1].src = 0;
-        graph.edge[1].dest = 2;
-        graph.edge[1].weight = 6;
-
-        graph.edge[2].src = 0;
-        graph.edge[2].dest = 3;
-        graph.edge[2].weight = 5;
-
-        graph.edge[3].dest = 1;
-        graph.edge[3].src = 3;
-        graph.edge[3].weight = 15;
-
-        graph.edge[4].src = 2;
-        graph.edge[4].dest = 3;
-        graph.edge[4].weight = 4;
-
-        graph.kruskalMST();
-
+        System.out.println("\nMinimum Spanning Tree Cost: " + ob.kruskalMST(arr));
     }
 }
